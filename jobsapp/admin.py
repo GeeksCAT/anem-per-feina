8512417d4c9564e3d39553a9bbd7dd272ca75b34
fullonic <somnium@riseup.net>
2020-10-29 17:07:14 +0100

    Add about us template and admin model

--- jobsapp/admin.py
+++ jobsapp/admin.py
@@ -1,4 +1,9 @@
-from django.contrib import admin  # noqa
+from ckeditor.widgets import CKEditorWidget
+
+from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 from jobsapp.models import Job
@@ -20,3 +25,26 @@ class JobAdmin(admin.ModelAdmin):
     ]
     list_filter = ["salary", "last_date", "created_at", "user"]
     date_hierarchy = "created_at"
+
+
+# Define a new FlatPageAdmin
+class FlatPageAdmin(FlatPageAdmin):
+    fieldsets = (
+        (None, {"fields": ("url", "title", "content")}),
+        (
+            _("Advanced options"),
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "enable_comments",
+                    "registration_required",
+                    "template_name",
+                ),
+            },
+        ),
+    )
+
+
+# Re-register FlatPageAdmin
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
