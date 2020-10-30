4dd53731576015cc35b0a748c30c224051da3405
fullonic <somnium@riseup.net>
2020-10-30 15:52:25 +0100

    Add contact us form

--- jobsapp/forms.py
+++ jobsapp/forms.py
@@ -1,7 +1,10 @@
 from django import forms
-from django.utils.translation import ugettext as _
+from django.core import validators
+from django.utils.translation import gettext as _
 
 from jobsapp.models import Job
+
+from .utils import contact_us_email
 
 
 class CreateJobForm(forms.ModelForm):
@@ -30,3 +33,18 @@ class CreateJobForm(forms.ModelForm):
         if commit:
             job.save()
         return job
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=128, help_text=_("Please insert your name"))
+    email = forms.EmailField(
+        max_length=256,
+        help_text=_("Please insert your email"),
+        validators=[validators.validate_email],
+    )
+    subject = forms.CharField(max_length=256, help_text=_("Reason why you are contact us"))
+    message = forms.CharField(max_length=2048, widget=forms.Textarea, help_text=_("Your message"))
+
+    def send_email(self) -> None:
+        # send email using the self.cleaned_data dictionary
+        contact_us_email(self.cleaned_data)
