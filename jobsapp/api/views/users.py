8ed14ed6b3a62b52b54dac669d232f7acc22000b
Xavier Marques <xaviermarques4f@gmail.com>
2020-10-31 13:50:27 +0100

    FIx import permission

--- jobsapp/api/views/users.py
+++ jobsapp/api/views/users.py
@@ -1,8 +1,9 @@
 from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from ...models import User
-from ..permissions import IsSelfOrReadOnly, AllowAny
+from ..permissions import IsSelfOrReadOnly
 from ..serializers import UserSerializer
+from rest_framework.permissions import AllowAny
 
 
 class UsersViewsList(ListCreateAPIView):
