bab8b0c70e18d6ece5dddb14d28070347046c177
Xavier Marques <xaviermarques4f@gmail.com>
2020-10-31 13:28:19 +0100

    Changes permissions /users

--- jobsapp/api/views/users.py
+++ jobsapp/api/views/users.py
@@ -1,14 +1,14 @@
 from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from ...models import User
-from ..permissions import IsSelfOrReadOnly
+from ..permissions import IsSelfOrReadOnly, AllowAny
 from ..serializers import UserSerializer
 
 
 class UsersViewsList(ListCreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsSelfOrReadOnly]
+    permission_classes = [AllowAny]
 
 
 class UsersViewDetails(CreateAPIView, RetrieveUpdateDestroyAPIView):
