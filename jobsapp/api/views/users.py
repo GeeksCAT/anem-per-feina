306fe708eaf067128089ea154e24f2d9ff204484
Xavier Marques <xaviermarques4f@gmail.com>
2020-10-31 11:42:55 +0100

    Change permissions of users endpoints

--- jobsapp/api/views/users.py
+++ jobsapp/api/views/users.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from ...models import User
 from ..serializers import UserSerializer
@@ -8,11 +8,11 @@ from ..serializers import UserSerializer
 class UsersViewsList(ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class UsersViewDetails(CreateAPIView, RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     print(queryset)
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
