69c7860181ca29edd53b5ce5bd1967ad9c4f1852
fullonic <somnium@riseup.net>
2021-01-24 16:10:28 +0100

    Style address form fields.
    Also removes the old location field.

--- tests/jobsapp/test_jopsapp.py
+++ tests/jobsapp/test_jopsapp.py
@@ -9,3 +9,14 @@ class TestExample:
     def test__example__ok(self) -> None:
         user = UserFactory(first_name="Geeks", last_name=".CAT")
         assert user.first_name == "Geeks"
+
+
+@pytest.mark.skip
+@pytest.mark.django_db
+def test_form_view(client, user_factory):
+    from django.shortcuts import reverse
+
+    breakpoint()
+    user = user_factory()
+    client.force_login(user=user)
+    req = client.get(reverse("jobs:employer-jobs-create"))
